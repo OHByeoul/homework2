9babbc42c2302bf8a1ca997473327c373abd9f9e
오한별 <suriessay@naver.com>
2019-12-25 21:47:19 +0900

    1. response 형식변경 (이전에 변경했는데 업 안됨)

---
+++ CCHomework/src/main/java/com/ccmedia/homework/model/ResponseContainer.java
@@ -0,0 +1,62 @@
+package com.ccmedia.homework.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.ccmedia.homework.util.MessageConstants;
+import com.google.gson.Gson;
+
+public class ResponseContainer<T> {
+	private T payload;
+	private PagingDTO pagingDTO;
+	private Gson gson;
+	private String errorMessage;
+
+	public ResponseContainer() {
+		gson = new Gson();
+		errorMessage = "not defined errmessage";
+	}
+
+	public PagingDTO getPagingDTO() {
+		return pagingDTO;
+	}
+
+	public void setPagingDTO(PagingDTO pagingDTO) {
+		this.pagingDTO = pagingDTO;
+	}
+
+	public T getPayload() {
+		return payload;
+	}
+
+	public void setPayload(T payload) {
+		this.payload = payload;
+	}
+
+	public Gson getGson() {
+		return gson;
+	}
+
+	public void setGson(Gson gson) {
+		this.gson = gson;
+	}
+
+	public String getErrorMessage() {
+		System.out.println(this.errorMessage);
+		return this.errorMessage;
+	}
+
+	public void setErrorMessage(String code) {
+		this.errorMessage = MessageConstants.getMessage(code);
+	}
+
+	public String getResponseToJson() {
+		List<Object> response = new ArrayList<Object>();
+		if(pagingDTO != null) {
+			response.add(pagingDTO);
+		}
+		response.add(payload);
+		return gson.toJson(response);
+	}
+
+}
